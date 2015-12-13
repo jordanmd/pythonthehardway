8dfefc62d053bdb7586d8a16903b027b7368bf4d
Michael Jordan <michael@jordanpad.com>
2015-12-12 21:19:58 -0600

    Added floating point math

--- ex3.py
+++ ex3.py
@@ -3,18 +3,18 @@
 # Printing an explanation for the calculations
 print "I will now count my chickens:"
 
-print "Hens", 25 + 30 / 6
+print "Hens", 25 + 30.0 / 6
 print "Roosters", 100 - 25 * 3 % 4
 
 # Print a new heading for the next calculation
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 # Print the results of a logical expression
 print "Is it true that 3 + 2 < 5 - 7?"
 
-print 3 +2 < 5 - 7
+print 3.0 +2 < 5 - 7
 
 # Printing a calculation after a print statement
 print "What is 3 + 2?", 3 + 2
