09d903dcd7d4108aedeb67de3b52db49bc67d7b7
Michael Jordan <michael@jordanpad.com>
2015-12-12 21:16:39 -0600

    Exercise 3 with comments added.

--- ex3.py
+++ ex3.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python
 
+# Printing an explanation for the calculations
 print "I will now count my chickens:"
 
-print "Hens", 25+30/6
-print "Roosters", 100-25*3%4
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
 
+# Print a new heading for the next calculation
 print "Now I will count the eggs:"
 
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Print the results of a logical expression
 print "Is it true that 3 + 2 < 5 - 7?"
 
 print 3 +2 < 5 - 7
 
+# Printing a calculation after a print statement
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
 
@@ -20,6 +24,7 @@ print "Oh, that's why it's False."
 
 print "How about some more."
 
+# Print more logical expressions.
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= 2
 print "Is it less or equal?", 5 <= -2
