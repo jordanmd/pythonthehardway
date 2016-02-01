664f6f69213fa2f171ff45546c039edf6ec43691
Michael Jordan <michael@jordanpad.com>
2016-01-31 21:58:34 -0600

    Corrected syntax in ex5 for sum at end. Added metric conversions for Corrected syntax in ex5 for sum at end. Added metric conversions for some units

--- ex5.py
+++ ex5.py
@@ -10,10 +10,12 @@ my_hair = 'Brown'
 
 print "Let's talk about %s." % my_name
 print "He's %d inches tall." % my_height
+print "... or %d in centimeters." % (my_height * 2.54)
 print "He's %d pounds heavy." % my_weight
+print "... or %d in kilograms." % (my_weight * 2.2)
 print "Actually, that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
