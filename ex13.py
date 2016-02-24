96b2bd8103c0dafa4511dd61434c973c1f686620
Michael Jordan <michael@jordanpad.com>
2016-02-23 22:33:12 -0600

    Ex. 13 with raw_input and append.

--- ex13.py
+++ ex13.py
@@ -4,7 +4,9 @@ from sys import argv
 
 script, first, second, third = argv
 
+myprefix = raw_input("What word shall I add each word you passed in? ")
+
 print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
+print "Your first variable is:", myprefix + first
+print "Your second variable is:", myprefix + second
+print "Your third variable is:", myprefix + third
