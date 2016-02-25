c6905940ffb249e6d22f3cc5104262d7c6f302f3
Michael Jordan <michael@jordanpad.com>
2016-02-24 21:39:56 -0600

    Ex. 14 complete

---
+++ ex14.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from sys import argv
+
+script, user_name, weather = argv
+prompt = '===> '
+
+print "Hi %s, I'm the %s script." % (user_name, script)
+print "Apparently, it is %s where you are." % weather
+print "I'd like to ask you a few question."
+print "Do you like me %s?" % user_name
+likes = raw_input(prompt)
+
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+Alright, so you said %r about liking me.
+You live in %r. Not sure where that is. 
+And you have a %r computer. Nice. 
+""" % (likes, lives, computer)
+
